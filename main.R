d16bcfd9a557cb0d2af5d98b350b1faf94ed8afc
kgolofit <kamilg6@o2.pl>
2015-01-20 23:40:42 +0100

    Szablon (?) jak bedzie wygladac nasz klasyfikator

--- main.R
+++ main.R
@@ -1,4 +1,47 @@
 # First file
+oneVsAll <- function(X,Y,FUN,...)
+{
+# Tu bedziemy tworzyc modele - tu stworzy sie macierz i klasyfikatory binarne.
+# Tu tez chyba bedzie ich trenowanie
+  
+  
+#   models <- lapply(unique(Y), function(x)
+#   {
+#     name <- as.character(x)
+#     .Target <- factor(ifelse(Y==name,name,'other'), levels=c(name, 'other'))
+#     dat <- data.frame(.Target, X)
+#     model <- FUN(.Target~., data=dat, ...)
+#     return(model)
+#   })
+#   names(models) <- unique(Y)
+#   info <- list(X=X, Y=Y, classes=unique(Y))
+#   out <- list(models=models, info=info)
+#   class(out) <- 'oneVsAll'
+#   return(out)
+}
+
+predict.oneVsAll <- function(object, newX=object$info$X, ...) 
+{
+# Tutaj podobnie - predict dla kazdego z binarnych klasyfikatorow i dla wyjscia odkodowac decyzje. 
+  
+#   stopifnot(class(object)=='oneVsAll')
+#   lapply(object$models, function(x)
+#   {
+#     predict(x, newX, ...)
+#   })
+}
+
+classify <- function(dat) 
+{
+# To chyba u nas niepotrzebne (?) 
+  
+#   out <- dat/rowSums(dat)
+#   out$Class <- apply(dat, 1, function(x) names(dat)[which.max(x)])
+#   out
+}
+
+####################################################################
+
 makeMatrix <- function(classNo = 2)
 {
   if(classNo < 3)
