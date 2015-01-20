54014899c95598c6dfa6929d785ac273c61324ff
MGJanicki <mgjanicki@gmail.com>
2015-01-20 01:29:00 +0100

    matrix creation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MOW-14Z
+Sposób u¿ycia:
+w konsoli wklepujesz source("main.R")
+mo¿esz bawiæ siê funkcjami makeMatrix i prepareRow - ¿eby zobaczyæ dzia³anie dorzuæ print(zmienna_wyjœciowa) przed ostatni¹ instrukcj¹ w funkcji
+have fun!
--- main.R
+++ main.R
@@ -1,1 +1,60 @@
 # First file
+makeMatrix <- function(classNo = 2)
+{
+  if(classNo < 3)
+  {
+    return()
+  }
+  
+  #specify rows length
+  rowLength <- 2^(classNo-1) - 1
+  
+  #sequence length is 2^classNo so it's easier to prepare rows
+  seqLength <- rowLength + 1
+  
+  #initialize a matrix to work with
+  retMatrix <- matrix(0, classNo, rowLength)
+  
+  #prepare each row and fill a matrix with it
+  for(i in 1:classNo)
+  {
+    retMatrix[i,] <- prepareRow(rowLength, seqLength)
+    #cut seqLength in half
+    seqLength <- seqLength / 2
+  }
+  
+  #return matrix for ECOC
+  retMatrix
+}
+
+prepareRow <- function(rowLength, seqLength)
+{
+  #initialize a row vector
+  row <- 1:rowLength
+  
+  if(rowLength <= seqLength)
+  {
+    for(i in 1:rowLength)
+    {
+      row[i] <- 1
+    }
+  }
+  else
+  {
+    #if there are some zero's we start with them
+    bit <- 0
+    
+    for(i in 1:rowLength)
+    {
+      row[i] <- bit
+      
+      if(i %% seqLength == 0)
+      {
+        #thanks to that we don't have new ifs
+        bit <- abs(bit - 1)
+      }
+    }
+  }
+  
+  row
+}
