23db2c6e21cb4b074fbea6064eef0be90c8b2354
MGJanicki <mgjanicki@gmail.com>
2015-01-22 03:43:46 +0100

    podstawy randomWalk

--- main.R
+++ main.R
@@ -42,7 +42,7 @@ classify <- function(dat)
 
 ####################################################################
 
-makeMatrix <- function(classNo = 3, naive = FALSE, removeColumns = FALSE)
+makeMatrix <- function(classNo = 3, columnNo = 0, naive = FALSE, removeColumns = FALSE)
 {
   if(classNo < 3)
   {
@@ -59,17 +59,18 @@ makeMatrix <- function(classNo = 3, naive = FALSE, removeColumns = FALSE)
     if(classNo >= 3 && classNo <= 11)
     {
       retMatrix <- exhaustiveCodes(classNo)
+      
+      #remove columns if it's nessecary
+      if(removeColumns == TRUE)
+      {
+        retMatrix <- removeColumns(retMatrix, columnNo)
+      }
+      
     }
-    #else TODO - random walk
-    #{
-    #  
-    #}
-    
-    #remove columns if it's nessecary
-    if(removeColumns == TRUE)
+    else
     {
-      retMatrix <- removeColumns(retMatrix)
-    }
+      retMatrix <- randomWalk(classNo, columnNo)
+    }  
     
   }
   
@@ -144,13 +145,14 @@ prepareRow <- function(rowLength, seqLength)
 }
 
 #function removes colums from ECOC matrix until Hamming Distance is 3 (minimum for ECOC codes)
-removeColumns <- function(ecocMatrix)
+#or until columnNo columns are left
+removeColumns <- function(ecocMatrix, columnNo)
 {
   #iteration variable
   i <- 1
   
   #iterate while index is in bounds
-  while(i <= length(ecocMatrix[1,]))
+  while(i <= length(ecocMatrix[1,]) && length(ecocMatrix[1,]) > columnNo)
   {
     tmpMatrix <- ecocMatrix[,-i]
     
@@ -167,6 +169,63 @@ removeColumns <- function(ecocMatrix)
   
   #return column with some columns removed
   ecocMatrix  
+}
+
+#function generates ecoc matrix using random walk algorithm
+randomWalk <- function(classNo, columnNo)
+{
+  #generate vector to initialize a matrix
+  iniVector <- sample(c(0, 1), classNo * columnNo, replace = TRUE)
+  
+  #create matrix
+  retMatrix <- matrix(iniVector, classNo, columnNo)
+  
+  minimalHD <- minimalHammingDetails(retMatrix)
+  
+  #improve matrix
+  while(minimalHD[3] < 3)
+  {
+    #find the columns that are identical
+    columns <- findColumns(retMatrix[minimalHD[1], ], retMatrix[minimalHD[2], ])
+    
+    #negate bits to enlarge the Hamming distance
+    retMatrix[minimalHD[1], columns[1]] <- abs(retMatrix[minimalHD[1], columns[1]] - 1)
+    retMatrix[minimalHD[2], columns[2]] <- abs(retMatrix[minimalHD[2], columns[2]] - 1)
+
+    print(retMatrix)
+    minimalHD <- minimalHammingDetails(retMatrix)
+  }
+  
+  retMatrix
+}
+
+findColumns <- function(wordA, wordB)
+{
+  column1 <- 0
+  column2 <- 0
+  i <- 1
+  
+  #iterate through columns to find same-value ones in both rows
+  while(i < length(wordA) && (column1 == 0 || column2 == 0))
+  {
+    if(wordA[i] == wordB[i])
+    {
+      if(column1 == 0)
+      {
+        column1 <- i
+      }
+      else
+      {
+        column2 <- i
+      }
+      i <- i + 1
+    }
+  }
+  
+  #return the columns
+  ret <- c(column1, column2)
+  
+  ret
 }
 
 #function counts Hamming distance beetwen two words (we expect words to have equal length)
@@ -208,10 +267,23 @@ decodeClass <- function(ecocMatrix, classWord)
 #function returns minimum Hamming distance between any two words in a given matrix
 minimalHamming <- function(ecocMatrix)
 {
+  retHamming <- minimalHammingDetails(ecocMatrix)
+  
+  retHamming[3]
+}
+
+#function returns minimum Hamming distance between any two words in a given matrix
+#additional return values are rows numbers
+minimalHammingDetails <- function(ecocMatrix)
+{
   classNo <- length(ecocMatrix[, 1])
   
   #there will be no greater distance than length of the word
   minDistance <- length(ecocMatrix[1, ])
+  
+  #rows to return
+  row1 <- 0
+  row2 <- 0
   
   for(i in 1:(classNo - 1))
   {
@@ -221,10 +293,12 @@ minimalHamming <- function(ecocMatrix)
       if(distance < minDistance)
       {
         minDistance <- distance
+        row1 <- i
+        row2 <- j
       }
     }
   }
   
   #return minDistance
-  minDistance
+  c(row1, row2, minDistance)
 }
